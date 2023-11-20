86437d95b2a3eab0bd49738e32dd559e3b77fcb8
Leo Feng <thcheetah777@gmail.com>
2023-11-20 19:27:51 +0200

    🔫 better guns

---
+++ gun_settings.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name GunSettings
+
+@export var sprite: Texture2D
+@export var bullet_size = 1.0
+@export var bullet_speed = 300
+@export var fire_rate = 5.0
+@export var spread = 8.0
+@export var damage = 1.0
+@export var offset = 2
+@export var kick = 5
+@export var expand = 1.5
--- player.gd
+++ player.gd
@@ -5,26 +5,38 @@ class_name Player
 
 @export var run_rotation = 30
 @export var run_rotation_smoothing = 250
+@export var gun_offset_smoothing = 20
+@export var gun_scale_smoothing = 10
+
+@export var gun: GunSettings
 
 @onready var sprite := $Sprite
 @onready var weapon_holder := $WeaponHolder
 @onready var weapon := $WeaponHolder/Weapon
 @onready var weapon_sprite := $WeaponHolder/Weapon/Sprite
 
-var target_rotation = 0
 var dust_scene = preload("res://dust.tscn")
 var bullet_scene = preload("res://bullet.tscn")
 
+var target_rotation = 0
+var original_weapon_scale = Vector2.ONE
+var next_time_to_fire = 0
+
 func _ready() -> void:
 	Globals.player = self
+	weapon_sprite.texture = gun.sprite
+	weapon.position.x = gun.offset
 
 func _process(delta: float) -> void:
 	sprite.rotation_degrees = move_toward(sprite.rotation_degrees, target_rotation,  run_rotation_smoothing * delta)
 
 	weapon_holder.look_at(get_global_mouse_position())
-	weapon_sprite.flip_h = not (weapon_holder.global_rotation_degrees > -90 and weapon_holder.global_rotation_degrees < 90)
+	weapon_sprite.flip_v = not (weapon_holder.global_rotation_degrees > -90 and weapon_holder.global_rotation_degrees < 90)
 
-	if Input.is_action_just_pressed("fire"):
+	weapon.position.x = move_toward(weapon.position.x, gun.offset, gun_offset_smoothing * delta)
+	weapon.scale = weapon.scale.move_toward(original_weapon_scale, gun_scale_smoothing * delta)
+
+	if Input.is_action_pressed("fire") and Time.get_ticks_msec() >= next_time_to_fire:
 		fire()
 
 func _physics_process(delta: float) -> void:
@@ -38,10 +50,16 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func fire() -> void:
+	next_time_to_fire = Time.get_ticks_msec() + 1000 / gun.fire_rate
+	weapon.position.x = gun.offset - gun.kick
+	weapon.scale = Vector2.ONE * gun.expand
+
 	var bullet = bullet_scene.instantiate() as Bullet
+	var spread = deg_to_rad(randf_range(-gun.spread, gun.spread))
 	bullet.global_position = weapon.global_position
-	bullet.look_at(get_global_mouse_position())
-	bullet.speed = 300
+	bullet.scale = Vector2.ONE * gun.bullet_size
+	bullet.look_at(get_global_mouse_position().rotated(spread))
+	bullet.speed = gun.bullet_speed
 	Globals.world.add_child(bullet)
 
 func _on_dust_timer_timeout() -> void:

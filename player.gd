2bca8d2c6e1d9e9d86d172ac7088857308be6dff
Leo Feng <thcheetah777@gmail.com>
2023-11-20 15:36:09 +0200

    gun aiming

--- player.gd
+++ player.gd
@@ -7,6 +7,8 @@ class_name Player
 @export var run_rotation_smoothing = 250
 
 @onready var sprite := $Sprite
+@onready var weapon_holder := $WeaponHolder
+@onready var weapon_sprite := $WeaponHolder/Weapon/Sprite
 
 var target_rotation = 0
 var dust_scene = preload("res://dust.tscn")
@@ -16,6 +18,8 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	sprite.rotation_degrees = move_toward(sprite.rotation_degrees, target_rotation,  run_rotation_smoothing * delta)
+	weapon_holder.look_at(get_global_mouse_position())
+	weapon_sprite.flip_h = not (weapon_holder.global_rotation_degrees > -90 and weapon_holder.global_rotation_degrees < 90)
 
 func _physics_process(delta: float) -> void:
 	var x_input = Input.get_axis("left", "right")

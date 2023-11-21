8575072c6f37be1660caa09302aac9cb039d3172
Leo Feng <thcheetah777@gmail.com>
2023-11-21 14:20:48 +0200

    ☠️ enemy hurt animations

--- enemy.gd
+++ enemy.gd
@@ -2,10 +2,23 @@ extends CharacterBody2D
 class_name Enemy
 
 @export var max_health = 8
+@export var scale_smoothing = 3
+@export var hurt_expand = 1.6
+
+@onready var sprite := $AnimatedSprite
 
 var health = max_health
+var original_scale = Vector2.ONE
+
+func _ready() -> void:
+	original_scale = scale
+
+func _process(delta: float) -> void:
+	scale = scale.move_toward(original_scale, scale_smoothing * delta)
 
 func take_damage(amount: float):
 	health -= amount
+	scale = hurt_expand * Vector2.ONE
+	sprite.play("hurt")
 	if health <= 0:
 		queue_free()
